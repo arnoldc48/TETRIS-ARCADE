2653ec483d7f478960e17fb8eba47ea85246b2ec
arnoldc48 <a20224863@pucp.edu.pe>
2024-12-12 15:27:30 -0500

    NOTES.h

--- NOTES.h
+++ NOTES.h
@@ -1,82 +1,45 @@
-#ifndef NOTES_H
-#define NOTES_H
+#define FCPU 16000000UL // Frecuencia del reloj del sistema
 
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
+// Frecuencias de las notas musicales
 #define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
 #define NOTE_B4  494
 #define NOTE_C5  523
 #define NOTE_D5  587
-#define NOTE_E5  659
+#define NOTE_A4  440
 #define NOTE_F5  698
 #define NOTE_G5  784
 #define NOTE_A5  880
-#define NOTE_C6  1047
-#define NOTE_A4  440
-// Define el silencio
 #define NOTE_NONE 0
 
-// Tamaños de las melodías
-#define MELODY_SIZE_EXISTENTE 33
-#define MELODY_SIZE_TETRIS 26
-#define MELODY_SIZE_LOSE 8
+// Identificadores de las notas musicales
+#define NOTA_SILENC 0
+#define NOTE_E52    1
+#define NOTE_B42    2
+#define NOTE_C52    3
+#define NOTE_D52    4
+#define NOTE_A42    5
+#define NOTE_F52    6
+#define NOTE_G52    7
+#define NOTE_A52    8
 
-#endif // NOTES_H
+
+// Duraciones de las notas (en milisegundos)
+#define NOTE_E5_DUR 8
+#define NOTE_B4_DUR 8
+#define NOTE_C5_DUR 8
+#define NOTE_D5_DUR 8
+#define NOTE_A4_DUR 8
+#define NOTE_A5_DUR 8
+#define NOTE_F5_DUR 8
+#define NOTE_G5_DUR 8
+
+const uint32_t tablaModulos[] = {
+    0,                    // Silencio
+    FCPU / NOTE_E5,       // E5
+    FCPU / NOTE_B4,       // B4
+    FCPU / NOTE_C5,       // C5
+    FCPU / NOTE_D5,       // D5
+    FCPU / NOTE_A4,       // A4
+    FCPU / NOTE_F5,       // F5
+    FCPU / NOTE_G5        // G5
+};
